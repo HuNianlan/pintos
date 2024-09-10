b00fb1947623139e8734d15997d1b2ba8e067df7
idnrrrrrrso <2697113701@qq.com>
2024-09-10 15:29:24 +0800

    can accept arguments, infinte loop in process_wait()

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -96,6 +96,8 @@ struct thread
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
+
+    int exit_status;
 #endif
 
     /* Owned by thread.c. */
--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,8 +3,32 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "userprog/pagedir.h"
+#include "threads/vaddr.h"
+
+#define STDIN 0
+#define STDOUT 1
+
+typedef int pid_t;
 
 static void syscall_handler (struct intr_frame *);
+
+bool is_valid_pointer (void *ptr);
+bool is_valid_buffer (const void *buffer, unsigned size);
+
+// void halt (void);
+void exit (int status);
+// pid_t exec (const char *cmd_line);
+// int wait (pid_t pid);
+// bool create (const char *file, unsigned initial_size);
+// bool remove (const char *file);
+// int open (const char *file);
+// int filesize (int fd);
+// int read (int fd, void *buffer, unsigned size);
+int write (int fd, const void *buffer, unsigned size);
+// void seek (int fd, unsigned position);
+// unsigned tell (int fd);
+// void close (int fd);
 
 void
 syscall_init (void) 
@@ -13,8 +37,119 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f UNUSED)
 {
-  printf ("system call!\n");
+  // printf ("system call!\n");
+  // thread_exit ();
+  uint32_t *esp = f->esp;
+  if (!is_valid_pointer (esp) || !is_valid_pointer (esp + 1)
+      || !is_valid_pointer (esp + 2) || !is_valid_pointer (esp + 3))
+    {
+      exit (-1);
+      return;
+    }
+  else
+    {
+      int syscall_number = *esp;
+      switch (syscall_number)
+        {
+        // case SYS_HALT:
+        //   halt ();
+        //   break;
+        case SYS_EXIT:
+          exit (*(esp + 1));
+          break;
+        // case SYS_EXEC:
+        //   f->eax = exec ((char *)*(esp + 1));
+        //   break;
+        // case SYS_WAIT:
+        //   f->eax = wait (*(esp + 1));
+        //   break;
+        // case SYS_CREATE:
+        //   f->eax = create ((char *)*(esp + 1), *(esp + 2));
+        //   break;
+        // case SYS_REMOVE:
+        //   f->eax = remove ((char *)*(esp + 1));
+        //   break;
+        // case SYS_OPEN:
+        //   f->eax = open ((char *)*(esp + 1));
+        //   break;
+        // case SYS_FILESIZE:
+        //   f->eax = filesize (*(esp + 1));
+        //   break;
+        // case SYS_READ:
+        //   f->eax = read (*(esp + 1), (void *)*(esp + 2), *(esp + 3));
+        //   break;
+        case SYS_WRITE:
+          f->eax = write (*(esp + 1), (void *)*(esp + 2), *(esp + 3));
+          break;
+        // case SYS_SEEK:
+        //   seek (*(esp + 1), *(esp + 2));
+        //   break;
+        // case SYS_TELL:
+        //   f->eax = tell (*(esp + 1));
+        //   break;
+        // case SYS_CLOSE:
+        //   close (*(esp + 1));
+        //   break;
+        default:
+          exit (-1);
+          break;
+        }
+    }
+}
+
+/* check ptr when accessing user memory */
+bool
+is_valid_pointer (void *ptr)
+{
+  if (ptr == NULL || !is_user_vaddr (ptr)
+      || pagedir_get_page (thread_current ()->pagedir, ptr) == NULL
+      || ptr < (void *)0x08048000)
+    return false;
+  return true;
+}
+
+void
+exit (int status)
+{
+  struct thread *t = thread_current ();
+  // struct list_elem *l = list_begin (&t->fd_list);
+  t->exit_status = status;
+  printf ("%s:exit(%d)\n", t->name, status);
+
   thread_exit ();
+}
+
+int
+write (int fd, const void *buffer, unsigned size)
+{
+  if (!is_valid_buffer (buffer, size))
+    exit (-1);
+
+  if (fd == STDOUT)
+    {
+      putbuf ((char *)buffer, (size_t)size);
+      // lock_release (&file_lock);
+      return size;
+    }
+  else if (fd == STDIN)
+    {
+      // lock_release (&file_lock);
+      return -1;
+    }
+    return -1;
+}
+
+bool
+is_valid_buffer (const void *buffer, unsigned size)
+{
+  void *ptr = (void*)buffer;
+  for (unsigned i = 0; i < size; i++)
+    {
+      if (!is_valid_pointer ((void *)ptr))
+        return false;
+      ptr++;
+    }
+  return true;
 }
